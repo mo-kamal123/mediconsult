0faa837c4a799b1853e2e65d44a0dae5df27ed9d
Mostafa Kamal <mk4086219@gmail.com>
2025-09-08 12:07:22 +0300

    implement main strucure

---
+++ src/app/routes/router.jsx
@@ -0,0 +1,4 @@
+import { createBrowserRouter } from 'react-router-dom';
+import Home from '../../features/main/pages/home';
+
+export const router = createBrowserRouter([{ path: '/', element: < Home /> }]);
